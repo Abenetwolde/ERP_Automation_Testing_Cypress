39154d03a5d696f82de73fd74bb8aa24ff4eab8e
Abnetwolde <tecnoabi4@gmail.com>
2023-06-04 23:34:43 -0700

    refactor

--- cypress/e2e/document.cy.js
+++ cypress/e2e/document.cy.js
@@ -2,23 +2,19 @@
 import Document from "../PageObjects/documentData.js"
 import Login from "../PageObjects/LoginPage.js"
 describe('documentRequest', () => {
-    before(() => {
-            cy.get('body').then(($body) => {
-                if ($body.text().includes('hiwot')) {
-                  return;
-                } else {
-                    const logUrl = Cypress.env('loginUrl')
-                  // User is not logged in, redirect to login page
-                  
-           
-         
-                  const loginObject= new Login()
-                  cy.visit(`${logUrl}`)
-                  loginObject.setUserName("hiwot")
-                  loginObject.setPassword(1234)
-                  loginObject.clickLogin();
-                }
-              });
+  before(() => {
+    cy.get('body').then(($body) => {
+      if ($body.text().includes('hiwot')) {
+        return;
+      } else {
+        const logUrl = Cypress.env('loginUrl')
+        const loginObject = new Login()
+        cy.visit(`${logUrl}`)
+        loginObject.setUserName("hiwot")
+        loginObject.setPassword(1234)
+        loginObject.clickLogin();
+      }
+    });
     //     cy.getCookie('JSESSIONID').then((cookie) => {
     //         if (cookie === null) {
     //             const loginUrl = Cypress.env('loginUrl')
@@ -30,54 +26,39 @@ describe('documentRequest', () => {
     //           }
 
     //   });   
+  })
+
+  it('DataDrivendDcumentRequestTest', () => {
+    const docUrl = Cypress.env('docUrl')
+    cy.fixture("documentData").then((data) => {
+      data.documentData.forEach((docData) => {
+        const docObject = new Document()
+        docObject.clickPluseIcon()
+        cy.wait(2000)
+        docObject.clickEmpNameTextFeild()
+        cy.wait(2000)
+        docObject.setEmpName(docData.employeeName)
+        cy.wait(5000)
+        docObject.clickenter()
+        // docObject.clickEmpName()
+        docObject.clickDocType()
+        cy.wait(2000)
+        docObject.clickselectDocument()
+        cy.wait(2000)
+        docObject.setRequestDate(docData.requestDate)
+        cy.wait(2000)
+        docObject.setPreparedOn(docData.preparedOn)
+        cy.wait(500)
+        docObject.setComment(docData.CommentGiven)
+        cy.wait(2000)
+        docObject.clickSave()
+        docObject.verifyDocumentRegister(docData.expectedResult);
+        cy.reload()
+        // cy.clearCookies()
+        // cy.clearLocalStorage()
+      })
     })
 
-    it('DataDrivendDcumentRequestTest', () => {
-        const docUrl = Cypress.env('docUrl')
-        cy.fixture("documentData").then((data) => {
-            data.documentData.forEach((docData) => {
-                const docObject = new Document()
-                // cy.visit(`${docUrl}`)
-          /*       const loginObject = new Login()
-                loginObject.setUserName("hiwot")
-                loginObject.setPassword(1234)
-                loginObject.clickLogin()
-              */
-                docObject.clickPluseIcon()
-                cy.wait(2000)
-                docObject.clickEmpNameTextFeild()
-                cy.wait(2000)
-                docObject.setEmpName(docData.employeeName)
-                cy.wait(5000)
-                docObject.clickenter()
-                // docObject.clickEmpName()
-                docObject.clickDocType()
-                cy.wait(2000)
-                docObject.clickselectDocument()
-                cy.wait(2000)
-                docObject.setRequestDate(docData.requestDate)
-                // docObject.clickenterRequestDate()
-                cy.wait(2000)
-                // docObject.clickSelectRequestDate()
-                // cy.wait(500)
-                docObject.setPreparedOn( docData.preparedOn)
-                cy.wait(500)
-                // docObject.clickSelectRequestDate()
-                // cy.wait(500)
-                docObject.setComment(docData.CommentGiven)
-                cy.wait(2000)
-                docObject.clickSave()
-                docObject.verifyDocumentRegister(docData.expectedResult);
-                cy.reload()
-                // cy.clearCookies()
-                // cy.clearLocalStorage()
-            })
-        })
-
-    })
-
-
-
-
+  })
 }
 )
