7b472412f6ec5a8c63d6935811310c879f2a06aa
Abnetwolde <tecnoabi4@gmail.com>
2023-06-23 01:52:33 -0700

    dynamic url

--- cypress/e2e/OrganizationInformation/payGrade.cy.js
+++ cypress/e2e/OrganizationInformation/payGrade.cy.js
@@ -1,15 +1,15 @@
 import Login from "../../PageObjects/LoginPage.js"
 const testData = require('../../fixtures/payGrade.json');
 
-
-describe('jobRegistaration Test', () => {
-    beforeEach(() => {
+console.log(testData.url)
+describe('PayGradeTest', () => {
+    before(() => {
         cy.get('body').then(($body) => {
             if ($body.text().includes('hiwot')) {
                 return;
             } else {
                 const loginObject = new Login()
-                cy.visit('https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/organization/payGrade.xhtml')
+                cy.visit(`${testData.url}`)
                 loginObject.setUserName("hiwot")
                 loginObject.setPassword(1234)
                 loginObject.clickLogin();
@@ -28,12 +28,8 @@ describe('jobRegistaration Test', () => {
       testData.payGradeData.forEach((data,i) => {
             it(`test datat ${data.testName}`, () => {
                
-                const loginObject = new Login()
-                cy.visit('https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/organization/payGrade.xhtml')
-                loginObject.setUserName("hiwot")
-                loginObject.setPassword(1234)
-                loginObject.clickLogin();
-                cy.reload()
+       
+             
 
             })
         })
