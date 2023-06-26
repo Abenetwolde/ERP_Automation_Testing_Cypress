cdbd13dba2334f8fde9ee3b2e61ade48af22dd20
Abnetwolde <tecnoabi4@gmail.com>
2023-06-26 00:53:57 -0700

    payGrade

--- cypress/e2e/OrganizationInformation/payGrade.cy.js
+++ cypress/e2e/OrganizationInformation/payGrade.cy.js
@@ -24,14 +24,53 @@ describe('PayGradeTest', () => {
         // });
 
     })
+    /*    // {
+      //   "testName":"Successfuly Test Data",
+      //   "salaryGrade": "E4",
+      //   "salaryStep": "9 ",
+      //   "salary": "5000",
+      //   "expectedResult":"Success"
+      
+      // },
+      // {
+      //   "testName":"Successfuly Test Data",
+      //   "salaryGrade": "E4",
+      //   "salaryStep": "9 ",
+      //   "salary": "5000",
+      //   "expectedResult":"Success"
+      
+      // }
+   */
 
       testData.payGradeData.forEach((data,i) => {
             it(`test datat ${data.testName}`, () => {
                
              cy.get('[id="formPayGrade:drdGrade_label"]').click()
+             cy.wait(500)
              cy.get(`li[data-label=${data.salaryGrade}]`).click();
+             cy.wait(500)
              cy.get('[id="formPayGrade:txtSalaryStep_label"]').click() 
-             cy.get(`li.ui-selectonemenu-item.ui-selectonemenu-list-item.ui-corner-all li[data-label=4]`).click();
+             cy.wait(500)
+             cy.get('.ui-selectonemenu-items-wrapper').should('be.visible')
+
+             cy.get(`div.ui-selectonemenu-items-wrapper li[data-label=${data.salaryStep}]`).click({ force: true })
+             cy.wait(500)
+             cy.get('[id="formPayGrade:txtSalary"]').type(data.salary) 
+             cy.wait(500)
+            
+             cy.get('[id="formPayGrade:btnAdd"]').click({ force: true }) 
+             cy.wait(1000)
+            //  cy.get('.ui-growl-message').should('be.visible').invoke('text').then((text) => {
+            //     const error = text.split('\n')[0].trim()
+            //     const message = text.split('\n')[1].trim()
+            //     expect(error).to.equal('Error!')
+            //     expect(message).to.equal('Duplicate entry salary step')
+            //   })
+            cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', data.expectedResult.mainText).should('contain', data.expectedResult.subText)
+
+             cy.wait(500)
+          
+           
             //  cy.get(`li[data-label=${data.salaryStep}]`).click();
             })
         })
