8a29a4a27b5fd39f9f611e17cddd9ec768b4a48a
Abnetwolde <tecnoabi4@gmail.com>
2023-06-29 02:42:43 -0700

    finsih SeveranceLiability test

--- cypress/e2e/IFRS/SeveranceLiability.cy.js
+++ cypress/e2e/IFRS/SeveranceLiability.cy.js
@@ -1,8 +1,8 @@
 import Login from "../../PageObjects/LoginPage.js"
-const testData = require('../../fixtures/needRequest.json');
+const testData = require('../../fixtures/SeveranceLiability.json');
 
 console.log(testData.url)
-describe('PayGradeTest', () => {
+describe('SeveranceLiability testing ', () => {
     beforeEach(() => {
         cy.get('body').then(($body) => {
             if ($body.text().includes('hiwot')) {
@@ -17,18 +17,53 @@ describe('PayGradeTest', () => {
         });
 
     })
-  
 
-    testData.needRequest.forEach((data, i) => {
+
+    testData.SeveranceLiability.forEach((data, i) => {
         it(`test datat ${data.testName}`, () => {
             cy.get('[id="frmLeaveBalance:txtYear"]').click()
-            cy.get('[id="frmLeaveBalance:txtYear"]').type("")
-       
-       
-        
-        
+            cy.get('[data-label="2015-2016"]').click()
+            cy.get('[id="frmLeaveBalance:tblAllBalance:j_idt77:filter"]').type(data.employid)
+            // cy.get('tbody tr:first-child td').each(($el) => {
+            //     cy.wrap($el).should('have.attr', 'role', 'gridcell')
+            //   })
+            // cy.get('[id="frmLeaveBalance:tblAllBalance_data"] tr').then(($rows) => {
+            //     const rowCount = $rows.length
+            //     for (let i = 0; i < rowCount; i++) {
+            //       cy.get('tr[data-ri="0"] td',{ timeout: 30000 }).eq(i).wait(2000)./* .should('have.attr', 'role', 'gridcell'). */contains('have.text', 'fc')
+            //     }
+            //   })
+            if (data.testId == 2) {
+                cy.get('tr.ui-widget-content.ui-datatable-empty-message td[colspan="7"]').should('contain', 'No records found.')
+            }
+            data.testId == 1 && cy.get('[id="frmLeaveBalance:tblAllBalance_data"] tr').then(($rows) => {
+                const rowCount = $rows.length
+                for (let i = 0; i < rowCount; i++) {
+                    cy.get('tr[data-ri="0"] ')
+                        // .eq(i)
+                        .within(() => {
+                            cy.get('td')
+                                .contains(data.expectedResult.mainText)
+                            // .then(($el) => {
+                            //   // $el is the td element that contains "fc" text
+                            //   // You can perform additional actions or assertions on $el here
+                            // })
+                        })
+                }
+            })
+            if (data.testId == 3){
+                cy.get('[id="frmLeaveBalance:tblAllBalance_rppDD"]').select("5")
+                cy.wait(3000)
+                cy.get('tr[data-ri="4"] td').contains(5)
+            }
+              
         })
+
+
+
+
+
+
     })
-}
-)
+})
 
