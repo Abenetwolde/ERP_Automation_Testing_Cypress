37ac59e1030df0fdd18bcbcfb99f281960d6151d
Abnetwolde <tecnoabi4@gmail.com>
2023-06-30 02:57:43 -0700

    issue on No of Emp Needed textfeild

--- cypress/e2e/HR Planinig/needRequest.cy.js
+++ cypress/e2e/HR Planinig/needRequest.cy.js
@@ -4,17 +4,30 @@ const testData = require('../../fixtures/HrPlaining/needRequest.json');
 console.log(testData.url)
 describe('PayGradeTest', () => {
     beforeEach(() => {
-        cy.get('body').then(($body) => {
-            if ($body.text().includes('hiwot')) {
-                return;
-            } else {
+        // cy.get('body').then(($body) => {
+        //     if ($body.text().includes('hiwot')) {
+        //         return;
+        //     } else {
+        //         const loginObject = new Login()
+        //         cy.visit(`${testData.url}`)
+        //         loginObject.setUserName("hiwot")
+        //         loginObject.setPassword(1234)
+        //         loginObject.clickLogin();
+        //     }
+        // });
+        cy.session("JSESSIONID", () => {
+            // Check if the "JSESSIONID" cookie is present
+            cy.getCookie("JSESSIONID").then((cookie) => {
+              // If the cookie is not present, log in
+              if (!cookie) {
                 const loginObject = new Login()
-                cy.visit(`${testData.url}`)
-                loginObject.setUserName("hiwot")
-                loginObject.setPassword(1234)
-                loginObject.clickLogin();
-            }
-        });
+                        cy.visit(`${testData.url}`)
+                        loginObject.setUserName("hiwot")
+                        loginObject.setPassword(1234)
+                        loginObject.clickLogin();
+              }
+            });
+          });
         // cy.fixture('PayGrade').then(data => {
         //     testData = data.payGradeData;
         // });
@@ -44,6 +57,7 @@ describe('PayGradeTest', () => {
 
     testData.needRequest.forEach((data, i) => {
         it(`test datat ${data.testName}`, () => {
+            cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/planning/needRequest.xhtml")
             cy.get('[id="frmSample:j_idt76"]').click()
             
             cy.get('[id="frmNeedRequest:somYear"]').click()
@@ -59,7 +73,8 @@ describe('PayGradeTest', () => {
             cy.get('[id="frmNeedRequest:sonJob"]').click()
             cy.wait(500)
             cy.get('[data-label="Administrative Assistant II"] ').eq(0).click({force: true})
-            cy.get('[id="frmNeedRequest:txtNoOfPosition"]').clear().type(2).trigger('change')
+            cy.get('[name="frmNeedRequest:txtNoOfPosition"]').click()
+            cy.get('[name="frmNeedRequest:txtNoOfPosition"]').type(2)
             cy.wait(500)
             cy.get('[id="frmNeedRequest:txtHowtobefilled"]').click()
             cy.wait(500)
