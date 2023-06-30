f8a7b07844983330a79cc74634a5a04f5b46c3b6
Abnetwolde <tecnoabi4@gmail.com>
2023-06-30 01:27:36 -0700

    avoid multiple login with session

--- cypress/e2e/IFRS/SeveranceLiability.cy.js
+++ cypress/e2e/IFRS/SeveranceLiability.cy.js
@@ -4,39 +4,25 @@ const testData = require('../../fixtures/SeveranceLiability.json');
 console.log(testData.url)
 describe('SeveranceLiability testing ', () => {
     beforeEach(() => {
-    
-        // cy.getCookie('session_id').then((cookie) => {
-        //     if (!cookie) {
-        //         const loginObject = new Login()
-        //                 cy.visit(`${testData.url}`)
-        //                 loginObject.setUserName("hiwot")
-        //                 loginObject.setPassword(1234)
-        //                 loginObject.clickLogin();
-        //     }
-        //     else{
-        //         cy.visit("https://172.21.35.239:8181/ERP-war/erp/ifrs/hrms/SeveranceLiability.xhtml")
-        //     }},
-           
-        // )
-        // cy.visit("https://172.21.35.239:8181/ERP-war/erp/ifrs/hrms/SeveranceLiability.xhtml")
-        cy.get('body').then(($body) => {
-            if ($body.text().includes('hiwot')) {
-                return;
-            } else {
+      cy.session("JSESSIONID", () => {
+            // Check if the "JSESSIONID" cookie is present
+            cy.getCookie("JSESSIONID").then((cookie) => {
+              // If the cookie is not present, log in
+              if (!cookie) {
                 const loginObject = new Login()
-                cy.visit(`${testData.url}`)
-                loginObject.setUserName("hiwot")
-                loginObject.setPassword(1234)
-                loginObject.clickLogin();
-            }
-        });
-
+                        cy.visit(`${testData.url}`)
+                        loginObject.setUserName("hiwot")
+                        loginObject.setPassword(1234)
+                        loginObject.clickLogin();
+              }
+            });
+          });
     })
 
 
     testData.SeveranceLiability.forEach((data, i) => {
         it(`test datat ${data.testName}`, () => {
-            // cy.visit("https://172.21.35.239:8181/ERP-war/erp/ifrs/hrms/SeveranceLiability.xhtml")
+            cy.visit("https://172.21.35.239:8181/ERP-war/erp/ifrs/hrms/SeveranceLiability.xhtml")
             cy.get('[id="frmLeaveBalance:txtYear"]').click()
             cy.get('[data-label="2015-2016"]').click()
             cy.get('[id="frmLeaveBalance:tblAllBalance:j_idt77:filter"]').type(data.employid)
--- cypress/support/auth.cmd.js
+++ cypress/support/auth.cmd.js
@@ -1,16 +1,10 @@
+Cypress.Cookies.defaults({
+  preserve: "session_id"
+});
 
-  Cypress.Commands.add('AuthCommand', () => {
-    const apiUrl = Cypress.env('apiUrl')
-    let cookie
-    cy.request({
-        url: `${apiUrl}`,
-    })
-        .then((c) => {
-            expect(c.status).to.eq(200)
-            cy.getCookie('JSESSIONID')
-            .should('exist')
-            .then((c) => {
-                cookie = c
-              })
-        })
-})
+// Preserve all cookies across tests
+Cypress.Cookies.defaults({
+  preserve: (cookie) => {
+    return true;
+  }
+});
