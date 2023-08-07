ffca891e2f8ff6e7802b36d554a604cd9c1b0f9b
Abnetwolde <tecnoabi4@gmail.com>
2023-08-07 04:56:18 -0700

    fix:parseXlsx

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,17 +3,17 @@
 // // const csv = require('csvtojson');
 //  const neatCSV = require('neat-csv');
 // const { data } = require('cypress/types/jquery');
-let testData = [];
+var testData 
 describe('Login Regression Test', () => {
   // Read test data from CSV file
  
-before(() => {
+beforeEach(() => {
   cy.parseXlsx('cypress/fixtures/ab.xlsx').then(jsonData=>{
-   const row=Cypress.$(jsonData[0].data).length 
-    cy.log(row)
-    })
-  })
-
+    testData=Cypress.$(jsonData[0].data)
+   
+  },
+  )})
+  
   // before( () => {
   //   cy.fixture('cypress/fixtures/test-data.csv').then(neatCSV).then(data=>{
   //   testData=data
@@ -26,9 +26,15 @@ before(() => {
   // });
   // console.log(testData);
   // Iterate over test data
-  testData.forEach((test) => {
-    it(`should login with username: ${test.username} and password: ${test.password}`, () => {
+ 
+  //  testData.forEach((test) => {
+    it("`should login with username: ${test.username} and password: ${test.password}`", () => {
       // Visit login page
+      // cy.parseXlsx('cypress/fixtures/ab.xlsx').then(jsonData=>{
+      //   const row=Cypress.$(jsonData[0].data).length 
+      //   console.log(row)
+      //  cy.log(row)
+      console.log(testData)
       cy.visit("https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/organization/OrganizationStruacture.xhtml")
         cy.get('[id="loginform:login-username"]').type(test.username)
         cy.wait(2000)
@@ -39,4 +45,4 @@ before(() => {
       cy.url().should('include', '/dashboard');
     });
   });
-});
+// })
