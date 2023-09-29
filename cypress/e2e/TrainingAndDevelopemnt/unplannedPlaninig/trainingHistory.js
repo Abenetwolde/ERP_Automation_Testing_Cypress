d18929bbd4fcebb9c618613a7cdbdf62d95d2a7d
Abnetwolde <tecnoabi4@gmail.com>
2023-09-29 01:36:56 -0700

    trainingHistory:update

--- cypress/e2e/TrainingAndDevelopemnt/unplannedPlaninig/trainingHistory.js
+++ cypress/e2e/TrainingAndDevelopemnt/unplannedPlaninig/trainingHistory.js
@@ -9,7 +9,7 @@ describe('unplannedTrainingRequest testing ', () => {
             cy.getCookie("JSESSIONID").then((cookie) => {
                 // If the cookie is not present, log in
                 if (!cookie) {
-                    cy.loginCommand(https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/training/trainingHistory.xhtml", 'hiwot', 1234);
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/training/trainingHistory.xhtml", 'hiwot', 1234);
                     //reusable login command
                 }
 
@@ -17,25 +17,30 @@ describe('unplannedTrainingRequest testing ', () => {
         });
     })
     // testData.SeveranceLiability.forEach((data, i) => {
-        it("` ${data.testName}`", () => {
-            cy.visit("https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/training/trainingHistory.xhtml")
-            cy.wait(500)
-            //Filter Criteria
-            cy.get('[id="frmUnplannedTrainingRequest:somFiliterByStatus_label"]').click()
-            //clcik Load Request List
-            cy.get('[data-label="Load Request List"]').click()
-            //clcik any row from the table
-            cy.get('table tbody tr').contains('td', 'someDep').click();
-            //
-            cy.get('[id="frmUnplannedTrainingRequest:txtParticipantId_input"]').type(123)
-            cy.wait(5000)
-            cy.get(`[data-item-value="123"]`).click()
-            cy.wait(500)
-            cy.get('[id="frmUnplannedTrainingRequest:btnAdd"]').click()
-            cy.wait(1000)
-            cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Error!").should('contain', "The number 0f employee must equal with participant!")
-        })
+    it("` ${data.testName}`", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/training/trainingHistory.xhtml")
+        cy.wait(500)
+        //Budget Year:
+        cy.get('[id="frmUnplannedTrainingRequest:plannedBudgetYear_label"]').click()
+        //clcik Load Request List
+        cy.get('[data-label="2014"]').click()
+        //Training Needs:
+        cy.get('[id="frmUnplannedTrainingRequest:plannedBudgetYear_label"]').click()
+        //clcik Load Request List
+        cy.get('[data-label="2014"]').click()
+        //clcik any row from the table
+        cy.get('table tbody tr').contains('td', 'fcmsAppFirstName').click();
+        //Training Status
+        cy.get('[id="frmUnplannedTrainingRequest:txtDecision_label"]').type(123)
+        cy.wait(5000)
+        cy.get(`[data-item-value="Attending"]`).click()
+        cy.wait(500)
+        //save button
+        cy.get('[id="frmUnplannedTrainingRequest:btnSave"]').click()
+        cy.wait(1000)
+        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Error!").should('contain', "The number 0f employee must equal with participant!")
     })
+})
 
 
 // })
