b2271b043cfb6554976cc5094e9605e946558f57
Abnetwolde <tecnoabi4@gmail.com>
2023-09-29 01:54:21 -0700

    trainingHistory:finish

--- cypress/e2e/TrainingAndDevelopemnt/unplannedPlaninig/trainingHistory.js
+++ cypress/e2e/TrainingAndDevelopemnt/unplannedPlaninig/trainingHistory.js
@@ -18,7 +18,7 @@ describe('unplannedTrainingRequest testing ', () => {
     })
     // testData.SeveranceLiability.forEach((data, i) => {
     it("` ${data.testName}`", () => {
-        cy.visit("https://172.21.35.239:8181/ERP-war/Login.xhtml?continue=https://172.21.35.239:8181/ERP-war/erp/hrms/training/trainingHistory.xhtml")
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/training/trainingHistory.xhtml")
         cy.wait(500)
         //Budget Year:
         cy.get('[id="frmUnplannedTrainingRequest:plannedBudgetYear_label"]').click()
@@ -27,18 +27,20 @@ describe('unplannedTrainingRequest testing ', () => {
         //Training Needs:
         cy.get('[id="frmUnplannedTrainingRequest:plannedBudgetYear_label"]').click()
         //clcik Load Request List
-        cy.get('[data-label="2014"]').click()
+        cy.get('[data-label="Architicture"]').click( {force: true})
         //clcik any row from the table
-        cy.get('table tbody tr').contains('td', 'fcmsAppFirstName').click();
+        cy.get('table[role="grid"] tbody tr').contains('td', 'fcmsAppFirstName').click();
+
+        // cy.get('table tbody tr').contains('td', 'fcmsAppFirstName').click();
         //Training Status
         cy.get('[id="frmUnplannedTrainingRequest:txtDecision_label"]').type(123)
         cy.wait(5000)
-        cy.get(`[data-item-value="Attending"]`).click()
+        cy.get(`[data-label="Attending"]`).click()
         cy.wait(500)
         //save button
         cy.get('[id="frmUnplannedTrainingRequest:btnSave"]').click()
         cy.wait(1000)
-        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Error!").should('contain', "The number 0f employee must equal with participant!")
+        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
     })
 })
 
