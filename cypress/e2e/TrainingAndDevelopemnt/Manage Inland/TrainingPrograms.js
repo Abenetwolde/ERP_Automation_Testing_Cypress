5be68b4dd099d800983a5bdb997b774587c4f1c4
Abnetwolde <tecnoabi4@gmail.com>
2023-10-02 02:09:14 -0700

    TrainingPrograms:update

---
+++ cypress/e2e/TrainingAndDevelopemnt/Manage Inland/TrainingPrograms.js
@@ -0,0 +1,60 @@
+// import Login from "../../PageObjects/LoginPage.js"
+// const testData = require('../../fixtures/IFRS/accuredleaveBalance.json');
+import "../../../support/auth.js"
+// console.log(testData.url)
+describe('TrainingPrograms testing ', () => {
+    beforeEach(() => {
+        cy.session("JSESSIONID", () => {
+            // Check if the "JSESSIONID" cookie is present
+            cy.getCookie("JSESSIONID").then((cookie) => {
+                // If the cookie is not present, log in
+                if (!cookie) {
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/training/TrainingPrograms.xhtml", 'hiwot', 1234);
+                    //reusable login command
+                }
+
+            })
+        });
+    })
+    // testData.SeveranceLiability.forEach((data, i) => {
+    it("TrainingPrograms Tests", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/training/TrainingPrograms.xhtml")
+        cy.wait(500)
+        //Filter Criteria:
+        cy.get('[id="frmInlandAndForiegnTraining:somFiliterByStatus"]').click()
+        //clcik Inland Education Programs 
+        cy.get('[data-label="Inland Education Programs "]').click()
+
+        cy.get('table[role="grid"] tbody tr').contains('td', 'Kebele 08').click();
+        cy.wait(5000)
+        // cy.get('table tbody tr').contains('td', 'fcmsAppFirstName').click();
+        //type InstitutionName
+        cy.get('[id="frmInlandAndForiegnTraining:txtxInstitutionName"]').type("a")
+        cy.wait(1000)
+        //type time 
+        cy.get('[id="frmInlandAndForiegnTraining:txtStartTime"]').type("11:10 AM")
+        cy.wait(1000)
+        //type Search name 
+        cy.get('[id="frmInlandAndForiegnTraining:selectedEmp_input"]').type("a")
+        cy.wait(1000)
+        cy.get('[data-item-value="Almaz"]').click()
+        cy.wait(500)
+        //type result
+        cy.get('[id="frmInlandAndForiegnTraining:txtTotalResult"]').type(20)
+        cy.wait(3000)
+        //selecte status
+        cy.get('[id="frmInlandAndForiegnTraining:txtstatus_label"]').click()
+        //clcik Inland Education Programs 
+        cy.get('[data-label="Completed"]').click()
+        //Add button
+        cy.get('[id="frmInlandAndForiegnTraining:btnadd"]').click()
+        cy.wait(5000)
+        cy.get('[id="frmInlandAndForiegnTraining:btnSave"]').click()
+        cy.wait(1000)
+        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
+    })
+})
+
+
+// })
+
