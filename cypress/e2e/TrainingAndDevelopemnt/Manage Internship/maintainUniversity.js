30dae6f13b353ea9fa135e22a715fa77f9bac24c
Abnetwolde <tecnoabi4@gmail.com>
2023-10-13 00:11:57 -0700

    maintainUniversity:finish

--- cypress/e2e/TrainingAndDevelopemnt/Manage Internship/maintainUniversity.js
+++ cypress/e2e/TrainingAndDevelopemnt/Manage Internship/maintainUniversity.js
@@ -2,14 +2,14 @@
 // const testData = require('../../fixtures/IFRS/accuredleaveBalance.json');
 import "../../../support/auth.js"
 // console.log(testData.url)
-describe('EducationPrograms testing ', () => {
+describe('maintainUniversity testing ', () => {
     beforeEach(() => {
         cy.session("JSESSIONID", () => {
             // Check if the "JSESSIONID" cookie is present
             cy.getCookie("JSESSIONID").then((cookie) => {
                 // If the cookie is not present, log in
                 if (!cookie) {
-                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/training/EducationPrograms.xhtml", 'hiwot', 1234);
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/training/maintainUniversity.xhtml", 'hiwot', 1234);
                     //reusable login command
                 }
 
@@ -17,33 +17,21 @@ describe('EducationPrograms testing ', () => {
         });
     })
     // testData.SeveranceLiability.forEach((data, i) => {
-    it("EducationPrograms Tests", () => {
-        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/training/EducationPrograms.xhtml")
+    it("maintainUniversity Tests", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/training/maintainUniversity.xhtml")
         cy.wait(500)
-        //Filter Criteria:
-        cy.get('[id="frmInlandAndForiegnTraining:somFiliterByStatus"]').click()
+        //Unversity
+        cy.get('[id="frmUniversity:drdUniversity_label"]').click()
         //clcik Inland Education Programs 
-        cy.get('[data-label="Inland Education Programs "]').click()
+        cy.get('[data-label="AASTUu ww"]').click()
 
-        cy.get('table[role="grid"] tbody tr').contains('td', 'Computer Science').click();
+        cy.get('table[role="grid"] tbody tr').contains('td', 'AASTUu ww').click();
         cy.wait(5000)
         // cy.get('table tbody tr').contains('td', 'fcmsAppFirstName').click();
         //search employee by its name
-        cy.get('[id="frmInlandAndForiegnTraining:selectedEmp_input"]').type("a")
-        cy.wait(5000)
-        cy.get('[data-item-value="Almaz"]').click()
-        cy.wait(500)
-        //type result
-        cy.get('[id="frmInlandAndForiegnTraining:txtTotalResult"]').type(20)
-        cy.wait(3000)
-        //selecte status
-        cy.get('[id="frmInlandAndForiegnTraining:txtstatus_label"]').click()
-        //clcik Inland Education Programs 
-        cy.get('[data-label="Completed"]').click()
-        //Add button
-        cy.get('[id="frmInlandAndForiegnTraining:btnadd"]').click()
-        cy.wait(5000)
-        cy.get('[id="frmInlandAndForiegnTraining:btnSave"]').click()
+        cy.get('[id="frmUniversity:txtOfficeNumber"]').clear().type("+251912345432")
+       
+        cy.get('[id="frmUniversity:btnSave"]').click()
         cy.wait(1000)
         cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
     })
