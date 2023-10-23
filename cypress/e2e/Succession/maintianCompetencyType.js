241fd2352439a9de03afa69fdbbecfcd2d18c187
Abenetwolde <abnetwoldedev@gmail.com>
2023-10-23 04:57:33 -0700

    maintianCompetencyType

--- cypress/e2e/Succession/maintianCompetencyType.js
+++ cypress/e2e/Succession/maintianCompetencyType.js
@@ -9,7 +9,7 @@ describe('Maintain Successor testing ', () => {
             cy.getCookie("JSESSIONID").then((cookie) => {
                 // If the cookie is not present, log in
                 if (!cookie) {
-                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/succession/maintianCompetency.xhtml", 'hiwot', 1234);
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/succession/maintianCompetencyType.xhtml", 'hiwot', 1234);
                     //reusable login command
                 }
 
@@ -18,20 +18,23 @@ describe('Maintain Successor testing ', () => {
     })
     // testData.SeveranceLiability.forEach((data, i) => {
     it("Maintain Successor Tests", () => {
-        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/succession/maintianCompetency.xhtml")
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/succession/maintianCompetencyType.xhtml")
         cy.wait(500)
-        //Search By:
-        cy.get('[id="frmRequest:drdCompetency_label"]').click()
-        //select Search By:
-        cy.get('[data-label="competency name"]').click()
+        //SrchCompetencyType
+        cy.get('[id="frmRequest:txtSrchCompetencyType"]').type("a")
+        //find button
+        cy.get('[id="frmRequest:btnSearch"]').click()
         cy.wait(500)
         cy.get('[id="frmRequest:autoCompetency_input"]').click("new")
         cy.wait(500)
-        cy.get('table[role="grid"] tbody tr').contains('td', 'new').click();
-        cy.wait(2000)
-        // update txtDescription
-        cy.get('[id="frmRequest:txtDescription"]').type("update")
+        cy.get('table[role="grid"] tbody tr').contains('td', 'ability to build model').click();
         cy.wait(500)
+        // update txtCompname_label
+        cy.get('[id="frmRequest:txtCompname_label"]').click()
+
+        cy.wait(500)
+        cy.get('[data-label="skill"]').click()
+
         cy.get('[id="frmRequest:btnSave"]').click()
         cy.wait(1000)
         cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
