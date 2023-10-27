8d3f50a30ab01d3ab2f72facde2664f08552b83f
Abenetwolde <abnetwoldedev@gmail.com>
2023-10-27 07:21:46 -0700

    probationCriteria:first test

---
+++ cypress/e2e/HR Routines/Performance Evaluation/Probation/probationCriteria.js
@@ -0,0 +1,39 @@
+// import Login from "../../PageObjects/LoginPage.js"
+// const testData = require('../../fixtures/IFRS/accuredleaveBalance.json');
+import "../../../../support/auth.js"
+// console.log(testData.url)
+describe('probationCriteria testing ', () => {
+    beforeEach(() => {
+        cy.session("JSESSIONID", () => {
+            // Check if the "JSESSIONID" cookie is present
+            cy.getCookie("JSESSIONID").then((cookie) => {
+                // If the cookie is not present, log in
+                if (!cookie) {
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/evaluation/probationCriteria.xhtml", 'hiwot', 1234);
+                    //reusable login command
+                }
+
+            })
+        });
+    })
+
+    it("probationCriteria Fist test", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/evaluation/probationCriteria.xhtml")
+        cy.wait(500)
+        //Criteria Name:
+        cy.get('[id="frmProbationCriteria:srcCriteriaName_label"]').click()
+        //clcik Load Request List
+        cy.get('[data-label="Knowledge"]').click()
+
+        cy.get('table[role="grid"] tbody tr').contains('td', 'Knowledge').click();
+        cy.wait(500)
+        //save button
+        cy.get('[id="frmProbationCriteria:btnSave"]').click()
+        cy.wait(1000)
+        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
+    })
+})
+
+
+// })
+
