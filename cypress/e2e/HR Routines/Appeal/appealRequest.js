421b1e8a54dbfd5fd1ebc2e83ebeeba1a9f33a39
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-02 04:55:31 -0700

    appealRequest: update button is not active issue

---
+++ cypress/e2e/HR Routines/Appeal/appealRequest.js
@@ -0,0 +1,38 @@
+
+import "../../../support/auth.js"
+// console.log(testData.url)
+describe('appealRequest first testing ', () => {
+    beforeEach(() => {
+        cy.session("JSESSIONID", () => {
+            // Check if the "JSESSIONID" cookie is present
+            cy.getCookie("JSESSIONID").then((cookie) => {
+                // If the cookie is not present, log in
+                if (!cookie) {
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/compliantHandling/appealRequest.xhtml", 'dagnachew', 1234);
+                    //reusable login command
+                }
+
+            })
+        });
+    })
+
+    it("appealRequest first test", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/compliantHandling/appealRequest.xhtml")
+        cy.wait(500)
+        //Filter Criteria:
+        cy.get('[id="frmSearch:somFiliterByStatus_label"]').click()
+        cy.wait(500)
+        cy.get('[data-label="Load Approved List"]').click()
+        cy.wait(500)
+        cy.get('table[role="grid"] tbody tr').contains('td', 'insa0012').click();
+        cy.wait(500)
+         //given comment 
+         cy.get('[id="frmAppealRequest:txtComment"]').type("given comment")
+        cy.wait(1000)
+        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
+    })
+})
+
+
+// })
+
