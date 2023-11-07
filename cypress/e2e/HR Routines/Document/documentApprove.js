62576ce72ffb5c3b5b179d6306c01a03c6700b62
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-07 00:35:14 -0800

    documentApprove:finish

--- cypress/e2e/HR Routines/Document/documentApprove.js
+++ cypress/e2e/HR Routines/Document/documentApprove.js
@@ -22,18 +22,22 @@ describe('documentApprove first testing ', () => {
         //Filter Criteria:
         cy.get('[id="frmDocumentapprove:somFiliterByStatus_label"]').click()
         cy.wait(500)
-        cy.get('[data-label="Load Approved List"]').click()
+        cy.get('[data-label="Load Request List"]').click()
         cy.wait(500)
-        cy.get('table[role="grid"] tbody tr').contains('td', '1').click({force: true});
+        cy.get('table[role="grid"] tbody tr').contains('td', 'eemelak').click({force: true});
         cy.wait(1000)
         cy.get('[id="frmDocumentapprove:txtDecsion_label"]').click()
         cy.wait(500)
         cy.get('[data-label="Approve"]').click()
         // cy.get('[id="frmDocumentRequest:txtCountry"]').type("Ethiopia")
         cy.wait(500)
-        
-        cy.get('[id="frmDocumentapprove:txtxreqdate"]').click("14/02/2016")
-
+        cy.get('[id="frmDocumentapprove:txtNoOfCopy"]').type("1")
+        cy.wait(500)
+        cy.get('[id="frmDocumentapprove:txtApprovetDate"]').clear().type("14/03/2016")
+        cy.wait(500)
+        cy.get('[id="frmDocumentapprove:txtxreqdate"]').type("14/02/2016")
+        cy.get('[id="frmDocumentapprove:txtxreqdate"]').click()
+        cy.get('[id="frmDocumentapprove:btnSave"]').click()
         cy.wait(1000)
         cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
     })
