64c7224e1f3bb28a4431109923ac2ab1ba28b14f
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-20 00:43:14 -0800

    Benefit:Medical:medicalCreditBill:finish

--- cypress/e2e/Benefits/Medical/medicalInstitution.js
+++ cypress/e2e/Benefits/Medical/medicalInstitution.js
@@ -17,8 +17,6 @@ describe('maintianInjuredEmployee first testing ', () => {
     })
 
     it("maintianInjuredEmployee first test", () => {
-
-
         cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/medical/medicalInstitution.xhtml")
         cy.wait(500)
         //Institution Name: 
@@ -55,64 +53,7 @@ describe('maintianInjuredEmployee first testing ', () => {
         cy.get('[id="frmMedicalInstitution:btnSave"]').click()
         cy.wait(500)
         cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
-
-        //Insurance Provider:
-        // cy.get('[id="frmRequest:Provider_label"]').click()
-        // cy.wait(500)
-        // cy.get('[data-label="AwashInsurance"]').eq(0).click( {force: true})
-        // //Accident Happened:
-        // cy.get('[id="frmRequest:drdhappened_label"]').click()
-        // cy.wait(500)
-        // cy.get('[data-label="On Duty"]').click()
-        // //Accident Place:
-        // //Accident Date:
-        // cy.get('[id="frmRequest:Accident"]').type("26/03/2015")
-        // cy.get('[id="frmRequest:Place"]').type("addis")
-        // cy.wait(500)
-        // //Description of accident:
-        // cy.get('[id="frmRequest:Discription"]').type("Description")
-        // cy.wait(500)
-        // cy.get('[id="frmRequest:btnSave1"]').click()
-        // cy.wait(500)
-        // cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
-
-
-        // cy.wait(2000)
-        // cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/medical/medicalInstitution.xhtml")
-        // cy.wait(500)
-        // //NOtification
-        // cy.get('[id="frmTop:cmdLinkNotf"]').click()
-        // //Filter by NAme
-        // cy.get('[id="frmTop:tblnotification:j_idt67:filter"]').type("Alm")
-        // cy.wait(2000)
-        // cy.get('tbody tr').contains('td', 'Almaz Sisay Bekele').click()
-
-        // //Hospital Name: :
-        // cy.get('[id="frmSuccessionplan:txtHospital"]').type("Hospital NAme")
-        // cy.wait(500)
-        // //Date::
-        // cy.get('[id="frmSuccessionplan:txtDate"]').type("05/03/2016")
-        // cy.get('td').contains('a', 'Hide').click();
-        // // cy.get('[id="frmSuccessionplan:txtDate"]').type("{enter}")
-        // cy.wait(500)
-        // //Name Of Injury/Disease: :
-        // cy.get('[id="frmSuccessionplan:txtaInjury"]').type("Name Of Injury Disease")
-        // cy.wait(500)
-        // //Physician Name: :
-        // cy.get('[id="frmSuccessionplan:txtPhysician"]').type("Physician Name")
-        // cy.wait(500)
-        // //time:
-        // cy.get('[id="frmSuccessionplan:txtTime_input"]').type("03:00:00")
-        // cy.wait(500)
-        // cy.get('body').click();
-        // //Sick Leave
-        // cy.get('[id="frmSuccessionplan:txtSick"]').focus().type(12)
-        // cy.wait(500)
-        // //Precentage Of Damage: 
-        // cy.get('[id="frmSuccessionplan:txtPresent"]').type(45)
-        // cy.wait(500)
-        // cy.get('[id="frmSuccessionplan:btnSave"]').click()
-        // cy.wait(500)
+        cy.wait(500)
         cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
 
     })
