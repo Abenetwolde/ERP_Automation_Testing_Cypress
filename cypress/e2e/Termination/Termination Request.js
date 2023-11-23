4369ce7bd01d75e71bf3b5110ca53ceb9325984f
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-23 00:58:19 -0800

    Termination Request:finish

--- cypress/e2e/Termination/Termination Request.js
+++ cypress/e2e/Termination/Termination Request.js
@@ -1,5 +1,5 @@
 
-import "../../../support/auth.js"
+import "../../support/auth.js"
 // console.log(testData.url)
 describe('medicaCreditBillApprove first testing ', () => {
     before(() => {
@@ -8,7 +8,7 @@ describe('medicaCreditBillApprove first testing ', () => {
             cy.getCookie("JSESSIONID").then((cookie) => {
                 // If the cookie is not present, log in
                 if (!cookie) {
-                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/overtime/OtRateSetup.xhtmll", 'hiwot', 1234);
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/termination/terminationRequest.xhtml", 'hiwot', 1234);
                     //reusable login command
                 }
 
@@ -16,43 +16,28 @@ describe('medicaCreditBillApprove first testing ', () => {
         });
     })
 
-    it("uniform first test", () => {
-        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/uniform/uniform.xhtml")
+    it("terminationRequest first test", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/termination/terminationRequest.xhtml")
         cy.wait(500)
-        // first remove OT Factor A to add new 
-        cy.get('[id="frmHeader:btnNew"]').click()
+        //+ icon 
+        cy.get('[id="frmToolBar:btnNew"]').click()
         cy.wait(500)
         //
-        cy.get('[id="frmUniform:somJob_label"]').click()
+        cy.get('[id="frmTermination:srcEmployeeId_input"]').type(123)
+        cy.wait(2000)
+        cy.get('[data-item-value="123"]').eq(0).click({force: true})
         cy.wait(500)
-        cy.get('[data-label="Accountant"]').eq(1).click({force: true})
+        //termination date
+        cy.get('[id="frmTermination:somResignType_label"]').click()
         cy.wait(500)
-        //CriteriaName_label
-        cy.get('[id="frmUniform:somCategory_label"]').click()
-        cy.wait(500)
-        cy.get('[data-label="Furnitures"]').eq(0).click()
-        cy.wait(500)
-        //somSubCategory_label
-        cy.get('[id="frmUniform:somSubCategory_label"]').click()
-        cy.wait(500)
-        cy.get('[data-label="chair"]').eq(0).click()
+        cy.get('[data-label="Resignation"]').eq(0).click()
         cy.wait(500)
         //somSubCategory_label
-        cy.get('[id="frmUniform:somUniformItem_label"]').click()
+        cy.get('[id="frmTermination:calResignDate"]').type("12/02/2017")
         cy.wait(500)
-        cy.get('[data-label="book"]').eq(0).click()
-        cy.wait(500)
-        //Quantity
-        cy.get('[id="frmUniform:txtQuantity"]').type(12)
-        cy.wait(500)
-        //somYear_label
-        cy.get('[id="frmUniform:somYear_label"]').click()
-        cy.wait(500)
-        cy.get('[data-label="1 years "]').click()
-        cy.wait(500)
-        cy.get('[id="frmUniform:btnAdd"]').click()
-        cy.wait(500)
-        cy.get('[id="frmUniform:btnSave"]').click()
+        cy.get('a[style="cursor: hand;color:#FF82AB;padding:1px; text-align:right;"]').contains('Hide').click()
+
+        cy.get('[id="frmTermination:btnSave"]').click()
         cy.wait(500)
         cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
     })
