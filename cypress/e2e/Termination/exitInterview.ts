117b3dc99ff08f7b4181f00a98cd516b0fc5d625
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-27 23:56:23 -0800

    exitInterview

---
+++ cypress/e2e/Termination/exitInterview.ts
@@ -0,0 +1,44 @@
+
+import "../../support/auth.d.ts"
+// console.log(testData.url)
+describe('retirementApprove first testing ', () => {
+    before(() => {
+        cy.session("JSESSIONID", () => {
+            // Check if the "JSESSIONID" cookie is present
+            cy.getCookie("JSESSIONID").then((cookie) => {
+                // If the cookie is not present, log in
+                if (!cookie) {
+                    cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/termination/exitInterview.xhtml", 'hiwot', 1234);
+                    //reusable login command
+                }
+
+            })
+        });
+    })
+
+    it("retirementApprove first test", () => {
+        cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/termination/exitInterview.xhtml")
+        cy.get('[id="frmExitInterview:srcEmpName"]').type("a")
+        cy.wait(500)
+        cy.get('[id="frmExitInterview:srcEmpName"]').click()
+        cy.wait(500)
+        cy.get('table[role="grid"] tbody tr').contains('td', 'Alemu Tadese Biru').click();
+        cy.wait(500)
+        cy.get('[id="frmExitInterview:srcEmpName"]').type("a")
+        cy.wait(500)
+        cy.get('[data-label="Approve"]').click()
+        cy.wait(500)
+        //termination date
+        cy.get('[id="frmRetirement:txtProcessedDate"]').clear().type("05/03/2016")
+        cy.wait(500)
+        cy.get('a[style="cursor: hand;color:#FF82AB;padding:1px; text-align:right;"]').contains('Hide').click()
+
+        cy.get('[id="frmRetirement:btnSave"]').click()
+        cy.wait(500)
+        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
+    })
+  
+})
+
+
+
