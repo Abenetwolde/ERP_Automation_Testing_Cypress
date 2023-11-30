3c9350403909c759f27a395dd84a4dbc105b2dc9
Abenetwolde <abnetwoldedev@gmail.com>
2023-11-29 23:52:44 -0800

    updateg

--- cypress/e2e/Admin/documentation.ts
+++ cypress/e2e/Admin/documentation.ts
@@ -5,8 +5,8 @@ describe('documentation Page Rendering Test ', () => {
     it("documentation Page Rendering Test", () => {
         cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/documentation.xhtml")
         cy.wait(500)
-        cy.get('introduction').should('be.visible')
-        cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
+        cy.get('#introduction > .page-header > a')
+       
     })
   
 })
