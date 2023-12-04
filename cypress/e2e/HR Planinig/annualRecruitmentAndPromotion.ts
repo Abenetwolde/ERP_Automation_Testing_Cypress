634016848f6694eb1a86e40e8d94d088f32dc0f7
Abenetwolde <abnetwoldedev@gmail.com>
2023-12-03 23:57:07 -0800

    revise clean up HR Planinig module

--- cypress/e2e/HR Planinig/annualRecruitmentAndPromotion.ts
+++ cypress/e2e/HR Planinig/annualRecruitmentAndPromotion.ts
@@ -1,32 +1,33 @@
 import { Validator } from "../../Helpers/Validator.js";
 import Login from "../../PageObjects/LoginPage.js"
-import "../../support/auth.js"
+import "../../support/auth.d.ts"
 const testData = require('../../fixtures/HrPlaining/demandAndSupplyAnalysis.json');
 
 console.log(testData.url)
 describe('PayGradeTest', () => {
-    beforeEach(() => {
-        cy.session("JSESSIONID", () => {
-            // Check if the "JSESSIONID" cookie is present
-            cy.getCookie("JSESSIONID").then((cookie) => {
-                // If the cookie is not present, log in
-                if (!cookie) {
-                    cy.loginCommand(testData.url, 'dagnachew', 1234);
-                    //reusable login command
-                }
+    // beforeEach(() => {
+    //     cy.session("JSESSIONID", () => {
+    //         // Check if the "JSESSIONID" cookie is present
+    //         cy.getCookie("JSESSIONID").then((cookie) => {
+    //             // If the cookie is not present, log in
+    //             if (!cookie) {
+    //                 cy.loginCommand(testData.url, 'dagnachew', 1234);
+    //                 //reusable login command
+    //             }
 
-            })
-        });
-    })
+    //         })
+    //     });
+    // })
 
    
-    testData.demandAndSupplyAnalysis.forEach((data, i) => {
+    testData.demandAndSupplyAnalysis.forEach((data: { testName: any; year: any; Department: any; remark: any; PreparedOn: string; }, i: any) => {
         it(`test data ${data.testName}`, () => {
-            cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/planning/annualRecruitmentAndPromotion.xhtml")
-            cy.wait(500)
+            cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/planning/annualRecruitmentAndPromotion.xhtml", 'dagnachew', 1234);
+            // cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/planning/annualRecruitmentAndPromotion.xhtml")
+            cy.wait(1000)
             // click the Year dropdown
             cy.get('[id="frmAnnualPlan:somYear_label"]').click()
-            cy.wait(500)
+            cy.wait(1000)
             // select year
             cy.get(`li[data-label="${data.year}"]`).eq(0).click({force: true});
             cy.wait(500)
