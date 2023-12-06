807b7ae5cfb62b6c6bf2eda554254e96f887302c
Abenetwolde <abnetwoldedev@gmail.com>
2023-12-06 06:19:36 -0800

    recruitmentRequests revise

--- cypress/e2e/Recruitment/recruitmentRequests.ts
+++ cypress/e2e/Recruitment/recruitmentRequests.ts
@@ -7,48 +7,33 @@ const testData = require('../../fixtures/Recruitment/recruitmentRequests.json');
 
 console.log(testData.url)
 describe('recruitmentRequests testing ', () => {
-    beforeEach(() => {
-        cy.session("JSESSIONID", () => {
-            // Check if the "JSESSIONID" cookie is present
-            cy.getCookie("JSESSIONID").then((cookie) => {
-                // If the cookie is not present, log in
-                if (!cookie) {
-                    cy.loginCommand(testData.url, 'hiwot', 1234);
-                    //reusable login command
-                }
 
-            })
-        });
-    })
 
 
     testData.recruitmentRequests.forEach((data, i) => {
         it(` ${data.testName}`, () => {
-            cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/recruitment/recruitmentRequests.xhtml")
+            cy.loginCommand(testData.url, 'hiwot', 1234);
             // click the plus icon
             cy.get('[id="frmSample:btnNew"]').click()
             // enter request id  
+            cy.wait(1000)
             cy.get('[id="frmRecruitment:txtRequester_input"]').type(`${data.Requester_id}`)
             cy.wait(4000)
-            cy.get('table.ui-autocomplete-table')
-                .contains('td', '123')
-                .click({force:true});
-
-            // cy.get(`[data-item-value="123"]`).click()
+            cy.get('[data-item-value="123"]').click()
             cy.wait(500)
             cy.get('[id="frmRecruitment:txtJob"]').click({ force: true })
-            // cy.get('tbody tr').first().click()
-
             // click job title dropdown 
             cy.get('[id="frmRecruitment:txtJob"]').click()
-            // select job title 
+            cy.wait(500)
+  
             cy.get(`[data-label="${data.jobTitle}"]`).click({ force: true })
             //type Number of employees requested
-            if (data.testId !== 2) { cy.get('[id="frmRecruitment:txtNoOfEmployee"]').type(data.txtNoOfEmployee) }
+            cy.wait(1000)
+            if (data.testId !== 2) { cy.get('[id="frmRecruitment:txtNoOfEmployee"]').type(`${data.txtNoOfEmployee}`) }
             cy.wait(500)
             // save Data
             cy.get('[id="frmRecruitment:btnSave"]').click()
-
+            cy.wait(500)
             if (data.testType == "validator") {
                 Validator(data)
             } else {
