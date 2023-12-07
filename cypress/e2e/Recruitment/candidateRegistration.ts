13a81f1ea939dfa48c224666836fca3293c5299f
Abenetwolde <abnetwoldedev@gmail.com>
2023-12-07 06:06:15 -0800

    candidateRegistration clear up

--- cypress/e2e/Recruitment/candidateRegistration.ts
+++ cypress/e2e/Recruitment/candidateRegistration.ts
@@ -1,28 +1,17 @@
 import Login from "../../PageObjects/LoginPage.js"
-import "../../support/auth.js"
+
+import "../../support/auth.d.ts"
 import { Validator } from "../../Helpers/Validator.js";
 const testData = require('../../fixtures/Recruitment/candidateRegistration.json');
 
 console.log(testData.url)
-describe('SeveranceLiability testing ', () => {
-    beforeEach(() => {
-        cy.session("JSESSIONID", () => {
-            // Check if the "JSESSIONID" cookie is present
-            cy.getCookie("JSESSIONID").then((cookie) => {
-                // If the cookie is not present, log in
-                if (!cookie) {
-                    cy.loginCommand(testData.url, 'hiwot', 1234);
-               
-                }
+describe('candidateRegistration testing ', () => {
 
-            })
-        });
-    })
 
  
     testData.candidateRegistration.forEach((data, i) => {
         it(` ${data.testName}`, () => {
-            cy.visit("https://172.21.35.239:8181/ERP-war/erp/hrms/recruitment/candidateRegistration.xhtml")
+            cy.loginCommand(testData.url, 'hiwot', 1234);
             // click the plus icon
             cy.get('[id="frmTop:btnNew"]').click()
             // click vacancy number dropdown 
