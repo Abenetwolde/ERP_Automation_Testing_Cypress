134d1594d89a29ef78d5114c66d4ffcc0eac2bc1
Abenetwolde <abnetwoldedev@gmail.com>
2023-12-21 06:24:07 -0800

    employee finish

--- cypress/e2e/Employee Profile/employee.ts
+++ cypress/e2e/Employee Profile/employee.ts
@@ -4,12 +4,13 @@ import "../../support/auth.d.ts"
 const testData = require('../../fixtures/HrPlaining/demandAndSupplyAnalysis.json');
 
 console.log(testData.url)
-describe('PayGradeTest', () => {
-    testData.demandAndSupplyAnalysis.forEach((data: { testName: any; year: any; Department: any; remark: any; PreparedOn: string; }, i: any) => {
-        it(`test data ${data.testName}`, () => {
+describe('employee registration test', () => {
+
+        it(`employee registration test `, () => {
             cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/employee/employee.xhtml", 'hiwot', 1234);
 
             cy.wait(1000)
+            cy.get('[id="frmTop:j_idt80"]').click()
             // txtEmployeeId
             cy.get('[id="frmEmployee:txtEmployeeId"]').type("8011")
             cy.wait(1000)
@@ -32,24 +33,34 @@ describe('PayGradeTest', () => {
             //somMaritalStats
             cy.get('[id="frmEmployee:somMaritalStatus_label"]').click()
             cy.wait(500)
-            cy.get(`li[data-label="Ato"]`).eq(0).click();
-            // clickDepartment dropdown
-            cy.get('[id="frmAnnualPlan:txtDirectorate_label"]').click()
+            cy.get(`li[data-label="Single"]`).eq(0).click();
+            // ctxtDepartment
+            cy.get('[id="frmEmployee:btnTree"]').click()
             cy.wait(500)
-            cy.get(`li[data-label="Ato"]`).eq(0).click();
+            cy.get('[id="frmEmployee:j_idt755:0"]').click({force: true})
+            cy.wait(500)
+    
+            cy.get('[id="frmEmployee:txtJob_label"]').click()
+            cy.wait(500)
+            cy.get(`li[data-label="Administrative Assistant II"]`).eq(0).click();
+            cy.wait(500)
+
+            cy.get('[id="frmEmployee:txtSalaryStep_label"]').click()
+            cy.wait(500)
+            cy.get(`li[data-label="1"]`).eq(0).click();
+            cy.wait(500)
+
+            cy.get('[id="frmEmployee:txtNationality_label"]').click()
+            cy.wait(500)
+            cy.get(`li[data-label="Ethiopian"]`).eq(0).click();
             cy.wait(500)
             // type txtRemark
-            cy.get('[id="frmAnnualPlan:txtRemark"]').type(`"${data.remark}"`)
-            // type Prepard on
-            cy.get('[id="frmAnnualPlan:txtxreqdate"]').type(data.PreparedOn)
+            cy.get('[id="frmEmployee:btnSave"]').click()
             cy.wait(500)
-            cy.get('[id="frmAnnualPlan:btnSave"]').click()
-            Validator(data)
+            cy.get('.ui-growl-message').should('be.visible').invoke('text').should('contain', "Success!")/* .should('contain', "The number 0f employee must equal with participant!") */
 
         })
     })
-}
-)
 
 
 
