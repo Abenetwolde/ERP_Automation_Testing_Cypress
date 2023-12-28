c9201342ebf9bb10b73242306bbed7b52f3332a8
Abenetwolde <abnetwoldedev@gmail.com>
2023-12-27 22:18:14 -0800

    leaveReturn

---
+++ cypress/e2e/Leave/leaveBalance.ts
@@ -0,0 +1,18 @@
+
+import "../../support/auth.d.ts"
+
+describe('leaveReturn test', () => {
+
+        it(`there should be a table test `, () => {
+            cy.loginCommand("https://172.21.35.239:8181/ERP-war/erp/hrms/leave/leaveBalance.xhtml", 'hiwot', 1234);
+            cy.get('[id="frmLeaveBalance:txtYear_label"]').click()
+            cy.wait(500)
+            cy.get(`li[data-label="2031-2032"]`).eq(0).click({force: true});
+            cy.get('tr').should('be.visible')
+
+        })
+    })
+
+
+
+
